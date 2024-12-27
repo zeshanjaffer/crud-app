566b5bccf02d0d619f04544c410ef2786b04d739
zeeshan jaffer <mazari.zeshan@gmail.com>
2024-12-27 06:56:30 +0500

    updated for vercel deployment

--- crm/settings.py
+++ crm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']  # Explicitly include localhost for development
+ALLOWED_HOSTS = ['.vercel.app']  # Explicitly include localhost for development
 
 
 # Application definition
